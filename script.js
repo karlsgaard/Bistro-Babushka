c50be2bcfa4cdaff20823fd8a543d5fea7a63f16
karlsgaard <isabellakarlsgaardrabe@gmail.com>
2020-02-12 16:27:49 +0100

    pop_up kommer frem

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const endpoint = "https://spreadsheets.google.com/feeds/list/17Dd7DvkPaFamNUdUKlrFgnH6POvBJXac7qyiS6zNRw0/od6/public/values?alt=json";
-
 let menu = [];
 
 let filter = "alle";
 
+const detalje = document.querySelector("#detalje");
+
+const endpoint = "https://spreadsheets.google.com/feeds/list/17Dd7DvkPaFamNUdUKlrFgnH6POvBJXac7qyiS6zNRw0/od6/public/values?alt=json";
+
 document.addEventListener("DOMContentLoaded", start);
+
+document.querySelector("#menuknap").addEventListener("touched", toggleMenu);
+
+document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
 window.addEventListener("load", sidenVises)
 
 function start() {
     loadData();
     buttons();
+    detalje.classList.add("luk")
 }
 
 async function loadData() {
@@ -18,9 +25,31 @@ async function loadData() {
     console.log(response);
     menu = await response.json();
     console.log(menu);
-
     visMenu();
 }
+
+//function visMenu() {
+//    const listevisning = document.querySelector(".data-container");
+//    const template = document.querySelector("template");
+//
+//    listevisning.innerHTML = "";
+//
+//    menu.feed.entry.forEach(mad => {
+//        if (filter == "alle" || filter == mad.gsx$kategori.$t) {
+//
+//            let klon = template.cloneNode(true).content;
+//
+//            klon.querySelector("img").src = "imgs/small/" + mad.gsx$billede.$t + "-sm.jpg";
+//            klon.querySelector("img").alt = "billede af" + mad.gsx$navn.$t;
+//
+//            klon.querySelector("#navn").textContent = mad.gsx$navn.$t;
+//            klon.querySelector("#p1").textContent = mad.gsx$kort.$t;
+//            klon.querySelector("#pris").textContent = mad.gsx$pris.$t;
+//
+//            listevisning.appendChild(klon);
+//        }
+//    })
+//}
 
 function visMenu() {
     const listevisning = document.querySelector(".data-container");
@@ -28,17 +57,24 @@ function visMenu() {
 
     listevisning.innerHTML = "";
 
+
     menu.feed.entry.forEach(mad => {
         if (filter == "alle" || filter == mad.gsx$kategori.$t) {
 
             let klon = template.cloneNode(true).content;
 
             klon.querySelector("img").src = "imgs/small/" + mad.gsx$billede.$t + "-sm.jpg";
+
             klon.querySelector("img").alt = "billede af" + mad.gsx$navn.$t;
 
             klon.querySelector("#navn").textContent = mad.gsx$navn.$t;
+
             klon.querySelector("#p1").textContent = mad.gsx$kort.$t;
-            klon.querySelector("#pris").textContent = mad.gsx$pris.$t;
+
+            klon.querySelector("#pris").textContent = mad.gsx$pris.$t + ",-";
+
+            klon.querySelector("article").addEventListener("click", visDetalje(mad));
+            klon.querySelector("article").addEventListener("click", skjulDetalje);
 
             listevisning.appendChild(klon);
         }
@@ -53,28 +89,53 @@ function buttons() {
 
 function filtrering() {
     filter = this.dataset.kategori;
+    visMenu();
     document.querySelector("h1").textContent = this.textContent;
     document.querySelectorAll(".filter").forEach(elm => {
         elm.classList.remove("valgt");
     })
 
     this.classList.add("valgt");
-    visMenu();
 }
+
+function skjulDetalje() {
+    detalje.classList.remove("luk");
+}
+
+function visDetalje(mad) {
+    detalje.querySelector("button").addEventListener("click", () => detalje.classList.add("luk"));
+    detalje.querySelector("img").src = `imgs/large/${mad.gsx$billede.$t}.jpg`;
+    detalje.querySelector("h2").textContent = mad.gsx$navn.$t;
+}
+
 
 function sidenVises() {
     console.log("sidenVises");
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
-function toggleMenu() {
-    console.log("toggleMenu");
-    document.querySelector("#menu").classList.toggle("hidden");
+//function toggleMenu() {
+//    console.log("toggleMenu");
+//    document.querySelector("#menu").classList.toggle("hidden");
+//
+//    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+//    if (erSkjult == true) {
+//        document.querySelector("#menuknap").textContent = "☰";
+//    } else {
+//        document.querySelector("#menuknap").textContent = "X";
+//    }
+//}
 
-    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-    if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
+function toggleMenu() {
+    console.log("menu pressed");
+    this.classList.toggle("menu_luk");
+    let x = document.querySelector("#filter_menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+
+        document.querySelector("#menu").textContent = "☰";
     } else {
-        document.querySelector("#menuknap").textContent = "X";
+        x.style.display = "block";
+        document.querySelector("#menu").textContent = "X";
     }
 }
